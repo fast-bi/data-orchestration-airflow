d6e6c2e2a6ca822ff5fc455b4790d09416ed7527
Looker_CI_CD <gediminas@terasky.com>
2023-12-04 14:42:11 +0200

    Init_Airflow_Image

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+
+set -o errexit
+catch() {
+    echo 'catching!'
+    if [ "$1" != "0" ]; then
+    # error handling goes here
+    echo "Error $1 occurred on $2"
+    fi
+}
+trap 'catch $? $LINENO' EXIT
+
+airflow_version="2.7.3"
+
+# docker build . \
+#   --pull \
+#   --build-arg BASE_AIRFLOW_IMAGE="apache/airflow:${airflow_version}" --build-arg AIRFLOW_VERSION="${airflow_version}" \
+#   --tag europe-central2-docker.pkg.dev/fast-bi-common/airflow/airflow-gcp:${airflow_version}
+
+# docker push europe-central2-docker.pkg.dev/fast-bi-common/airflow/airflow-gcp:${airflow_version}
+
+docker buildx build . \
+  --pull \
+  --build-arg BASE_AIRFLOW_IMAGE="apache/airflow:${airflow_version}" --build-arg AIRFLOW_VERSION="${airflow_version}" \
+  --tag europe-central2-docker.pkg.dev/fast-bi-common/airflow/airflow-gcp:${airflow_version} \
+  --platform linux/amd64
+
+docker push europe-central2-docker.pkg.dev/fast-bi-common/airflow/airflow-gcp:${airflow_version}
+
+##
+##pip install --index-url https://europe-central2-python.pkg.dev/fast-bi-common/bi-platform-pypi-packages/simple/ fast-bi-dbt-runner==0.0.1
